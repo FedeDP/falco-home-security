da295e25df6eb078ad804db3d1894d1083ee5ecd
Federico Di Pierro <nierro92@gmail.com>
2021-11-30 16:51:42 +0100

    Become smarter.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>
    
    Co-authored-by: Leonardo Grasso <me@leonardograsso.com>
    Co-authored-by: Jason Dellaluce <jasondellaluce@gmail.com>
    Co-authored-by: Lorenzo Susini <loresuso@users.noreply.github.com>
    Co-authored-by: andreabonanno <andreabonanno@users.noreply.github.com>
    Co-authored-by: deepskyblue86 <angelopuglisi86@gmail.com>

--- plugin/main.go
+++ plugin/main.go
@@ -13,14 +13,74 @@ import (
 	"gocv.io/x/gocv"
 	"image"
 	"image/color"
+	"math"
 	"os"
 	"strconv"
 )
 
-type Blob struct {
-	class ClassID
-	confidence float32
+type Ring struct {
+	vals []float32
+	cursor int
 }
+
+func (r *Ring) Push(val float32) {
+	r.vals[r.cursor] = val
+	r.cursor++
+	r.cursor %= len(r.vals)
+}
+
+func NewRing(size int) *Ring {
+	return &Ring{
+		vals:   make([]float32, size, size),
+		cursor: 0,
+	}
+}
+
+type ClassHistogram map[ClassID]*Ring
+
+type BlobPosition struct {
+	left  int
+	top int
+	right int
+	bottom int
+}
+
+type BlobCenter struct {
+	x int
+	y int
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func (bp BlobPosition) Center() BlobCenter {
+	x := (bp.right - bp.left) / 2
+	y := (bp.bottom - bp.top) / 2
+	return BlobCenter{x, y}
+}
+
+func (bc BlobCenter) Near(other BlobCenter) int {
+	const threshold = 10
+
+	if Abs(bc.x - other.x) < threshold &&
+		Abs(bc.y - other.y) < threshold {
+
+		return (bc.x - other.x) + Abs(bc.y - other.y)
+	}
+	return math.MaxInt
+}
+
+type Blob struct {
+	position BlobPosition
+	cumulativeConfidence ClassHistogram
+	nframes int
+}
+
+type Blobs []Blob
 
 // See https://tech.amikelive.com/node-718/what-object-categories-labels-are-in-coco-dataset/
 type ClassID int
@@ -29,6 +89,8 @@ const (
 	Cat = 17
 	Dog = 18
 )
+
+var Classes = []ClassID{ Human, Cat, Dog }
 
 func (c ClassID) String() string {
 	switch c {
@@ -50,8 +112,8 @@ func (c ClassID) Known() bool {
 	return false
 }
 
-func (c ClassID) Color() color.RGBA {
-	switch c {
+func (b Blob) Color() color.RGBA {
+	switch b.cumulativeConfidence.Winner() {
 	case Human:
 		return color.RGBA{B: 255}
 	case Cat:
@@ -60,6 +122,39 @@ func (c ClassID) Color() color.RGBA {
 		return color.RGBA{R: 255}
 	}
 	return color.RGBA{}
+}
+
+func (b Blob) Mean(class ClassID) float32 {
+	sum := b.cumulativeConfidence.Sum(class)
+
+	div := b.nframes
+	if div > 8 {
+		div = 8
+	}
+	return sum / float32(div)
+}
+
+func (ch ClassHistogram) Sum(class ClassID) float32 {
+	var sum float32
+	for _, val := range ch[class].vals {
+		sum += val
+	}
+	return sum
+}
+
+func (ch ClassHistogram) Winner() ClassID {
+	var (
+		maxVal float32
+		maxClass ClassID
+	)
+	for class := range ch {
+		sum := ch.Sum(class)
+		if sum > maxVal {
+			maxVal = sum
+			maxClass = class
+		}
+	}
+	return maxClass
 }
 
 func main() {
@@ -123,6 +218,7 @@ func main() {
 
 	fmt.Printf("Start reading device: %v\n", videosource)
 
+	var blobs []Blob
 	for {
 		if ok := capture.Read(&img); !ok {
 			fmt.Printf("Device closed: %v\n", videosource)
@@ -141,7 +237,7 @@ func main() {
 		// run a forward pass thru the network
 		prob := net.Forward("")
 
-		performDetection(&img, prob)
+		blobs = performDetection(&img, prob, blobs)
 
 		prob.Close()
 		blob.Close()
@@ -158,28 +254,65 @@ func main() {
 // where N is the number of detections, and each detection
 // is a vector of float values
 // [batchId, classId, confidence, left, top, right, bottom]
-func performDetection(frame *gocv.Mat, results gocv.Mat) []Blob {
+func performDetection(frame *gocv.Mat, results gocv.Mat, oldBlobs Blobs) Blobs {
 	var blobs []Blob
 	for i := 0; i < results.Total(); i += 7 {
 		confidence := results.GetFloatAt(0, i+2)
 		if confidence > 0.5 {
-			left := int(results.GetFloatAt(0, i+3) * float32(frame.Cols()))
-			top := int(results.GetFloatAt(0, i+4) * float32(frame.Rows()))
-			right := int(results.GetFloatAt(0, i+5) * float32(frame.Cols()))
-			bottom := int(results.GetFloatAt(0, i+6) * float32(frame.Rows()))
+			pos := BlobPosition{
+				left: int(results.GetFloatAt(0, i+3) * float32(frame.Cols())),
+				top: int(results.GetFloatAt(0, i+4) * float32(frame.Rows())),
+				right: int(results.GetFloatAt(0, i+5) * float32(frame.Cols())),
+				bottom: int(results.GetFloatAt(0, i+6) * float32(frame.Rows())),
+			}
 			classId := int(results.GetFloatAt(0, i+1))
 
 			c := ClassID(classId)
 			if c.Known() {
-				blobs = append(blobs, Blob{
-					class: c,
-					confidence: confidence,
-				})
-				status := fmt.Sprintf("type: %v, confidence: %v\n", c.String(), confidence)
-				gocv.PutText(frame, status, image.Pt(10, 20 * len(blobs)), gocv.FontHersheyPlain, 1.0, c.Color(), 2)
-				gocv.Rectangle(frame, image.Rect(left, top, right, bottom), c.Color(), 2)
+				blob := oldBlobs.findNearest(pos.Center())
+				if blob == nil {
+					fmt.Println("found new blob!")
+					// New blob!
+					blob = &Blob{
+						position: pos,
+						cumulativeConfidence: make(map[ClassID]*Ring),
+					}
+					for _, class := range Classes {
+						blob.cumulativeConfidence[class] = NewRing(8)
+					}
+				} else {
+					fmt.Println("found old blob!")
+					blob.position = pos
+				}
+				blob.nframes++
+				blob.cumulativeConfidence[c].Push(confidence)
+				blobs = append(blobs, *blob)
+
+				if blob.nframes > 3 {
+					status := fmt.Sprintf("type: %v, confidence: %v, nframes: %v", c.String(), blob.Mean(blob.cumulativeConfidence.Winner()), blob.nframes)
+					gocv.PutText(frame, status, image.Pt(10, 20*len(blobs)), gocv.FontHersheyPlain, 1.0, blob.Color(), 2)
+					gocv.Rectangle(frame, image.Rect(pos.left, pos.top, pos.right, pos.bottom), blob.Color(), 2)
+				}
 			}
 		}
 	}
 	return blobs
+}
+
+func (bb Blobs) findNearest(center BlobCenter) *Blob {
+	if bb == nil {
+		return nil
+	}
+	var (
+		minVal = math.MaxInt
+		minValBlob *Blob
+	)
+	for _, b := range bb {
+		val := b.position.Center().Near(center)
+		if minVal > val {
+			minVal = val
+			minValBlob = &b
+		}
+	}
+	return minValBlob
 }
