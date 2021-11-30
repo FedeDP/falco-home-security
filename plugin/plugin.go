05bc0baf90d77a0a924b956c82c546b416f9591e
Leonardo Grasso <me@leonardograsso.com>
2021-11-30 18:17:31 +0100

    wip(plugin): gocv config
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -2,26 +2,40 @@ package main
 
 import (
 	"encoding/gob"
+	"encoding/json"
 	"fmt"
 	"io"
+	"strconv"
 	"time"
 
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/extractor"
 	"github.com/falcosecurity/plugin-sdk-go/pkg/sdk/plugins/source"
+	"gocv.io/x/gocv"
 )
 
 type VideoPlugin struct {
 	plugins.BasePlugin
+	cfg *initConfig
 }
 
 type VideoInstance struct {
 	source.BaseInstance
+	capture *gocv.VideoCapture
+	net     *gocv.Net
 }
 
 // VideoEvent represents the event payload to be serialized
 type VideoEvent struct {
+}
+
+type initConfig struct {
+	VideoSource string `json:"videoSource"`
+	Model       string `json:"model"`
+	Backend     string `json:"backend"`
+	Target      string `json:"target"`
+	NetConfig   string `json:"netConfig"`
 }
 
 func init() {
@@ -48,6 +62,14 @@ func (m *VideoPlugin) Info() *plugins.Info {
 // Init initializes this plugin with a given config string, which is unused
 // in this example. This method is mandatory for source plugins.
 func (m *VideoPlugin) Init(config string) error {
+	cfg := initConfig{}
+
+	err := json.Unmarshal([]byte(config), &cfg)
+	if err != nil {
+		return err
+	}
+	m.cfg = &cfg
+
 	return nil
 }
 
@@ -85,7 +107,38 @@ func (m *VideoPlugin) Extract(req sdk.ExtractRequest, evt sdk.EventReader) error
 // Open opens the plugin source and starts a new capture session (e.g. stream
 // of events), creating a new plugin instance.
 func (m *VideoPlugin) Open(params string) (source.Instance, error) {
-	return &VideoInstance{}, nil
+	backend := gocv.ParseNetBackend(m.cfg.Backend)
+	target := gocv.ParseNetTarget(m.cfg.Target)
+
+	// open capture device (webcam or file)
+	var (
+		capture *gocv.VideoCapture
+		err     error
+	)
+
+	// If it is a number, open a video capture from webcam, else from file
+	id, err := strconv.Atoi(m.cfg.VideoSource)
+	if err == nil {
+		capture, err = gocv.OpenVideoCapture(id)
+	} else {
+		capture, err = gocv.VideoCaptureFile(m.cfg.VideoSource)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	// open DNN object tracking model
+	net := gocv.ReadNet(m.cfg.Model, m.cfg.NetConfig)
+	if net.Empty() {
+		return nil, fmt.Errorf("Error reading network model from : %v %v", m.cfg.Model, m.cfg.NetConfig)
+	}
+	net.SetPreferableBackend(backend)
+	net.SetPreferableTarget(target)
+
+	return &VideoInstance{
+		capture: capture,
+		net:     &net,
+	}, nil
 }
 
 // String produces a string representation of an event data produced by the
@@ -127,12 +180,10 @@ func (m *VideoInstance) NextBatch(pState sdk.PluginState, evts sdk.EventWriters)
 //
 // }
 
-// Close is gets called by the SDK when the plugin source capture gets closed.
-// This is useful to release any open resource used by each plugin instance.
-// This method is optional for source plugins.
-// func (m *VideoInstance) Close() {
-//
-// }
+func (m *VideoInstance) Close() {
+	m.capture.Close()
+	m.net.Close()
+}
 
 // Destroy is gets called by the SDK when the plugin gets deinitialized.
 // This is useful to release any open resource used by the plugin.
